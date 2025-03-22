21732fb981886812bc70f45d331168b8ae69f8ae
Rahmonaliyev Abdulaziz <142968268+RahmonaliyevAbdulaziz@users.noreply.github.com>
2025-03-23 00:00:21 +0500

    changed logo because of not displatg on vercel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav  className="sticky top-0 z-[9999] w-full backdrop-blur-sm py-5 bg-black/20 border-b border-b-white/40">
       <div className="flex justify-between items-center max-w-[1240px] px-4 mx-auto">
         <div>
-          <Link href="/" className="text-2xl text-white"><img src="/nkb2.svg" className="w-[70px] h-auto" alt="logo nkb" /></Link>
+          <Link href="/" className="text-2xl text-white">NKB</Link>
         </div>
         <ul className="flex items-center gap-6 text-white"> 
           <li className="after:content['']">
